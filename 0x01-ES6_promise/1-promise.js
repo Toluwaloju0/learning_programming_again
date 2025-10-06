096a01ac4006dd294e2c799a2b6d4686143eb67c
Toluwaloju0 <tolukayode2017@gmail.com>
2025-10-06 17:02:46 +0100

    Promises in javascript has been fun

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((res, rej) => {
+    if (success === true) {
+      res({
+        status: 200,
+        body: 'sucess',
+      });
+    } else { rej(new Error('The fake API is not working currently')); }
+  });
+}
