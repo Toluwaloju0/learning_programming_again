c2d9d0e4524f83b5ce44076f12b41dd8df9c2f0a
Toluwaloju0 <tolukayode2017@gmail.com>
2025-10-14 22:43:38 +0100

    ES6 classes has been fun to learn

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,24 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() { return this._name; }
+
+  set name(value) {
+    if (typeof (value) !== 'string') { throw TypeError('Name must be a string'); }
+    this._name = value;
+  }
+
+  get code() { return this._code; }
+
+  set code(value) {
+    if (typeof (value) !== 'string') { throw TypeError('code must be a string containing a symbol'); }
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
